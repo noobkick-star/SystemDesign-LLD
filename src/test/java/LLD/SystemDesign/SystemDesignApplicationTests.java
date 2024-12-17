40b6361424b21bd083b644ce598680505d2b7f2d
ankitsingh25996@gmail.com <ankitsingh25996@gmail.com>
2024-12-18 01:13:49 +0530

    initial commit

---
+++ src/test/java/LLD/SystemDesign/SystemDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package LLD.SystemDesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
